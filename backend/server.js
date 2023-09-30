f4c2692cadbc04c71ca6a2ecc2e30ff8b9aeed50
Mico Latorre Gaëtano <38907982+Gaet8802@users.noreply.github.com>
2023-09-30 02:58:47 +0200

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const { chats } = require("./data/data");
+const dotenv = require("dotenv");
+
+const app = express();
+dotenv.config();
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+app.get("/api/chats", (req, res) => {
+  res.send(chats);
+  console.log(req);
+});
+
+app.get("/api/chats/:id", (req, res) => {
+  //  console.log(req.params.id);
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
+});
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
