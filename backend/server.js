1fa7afedbda0127cdd3bd79c650a4622856ad0ce
Mico Latorre Gaëtano <38907982+Gaet8802@users.noreply.github.com>
2023-12-04 15:33:16 +0100

    up: build failed trying other build settings

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ if (process.env.NODE_ENV === "production") {
   });
 } else {
   app.get("/", (req, res) => {
-    res.send("API is running shitty");
+    res.send("API is running successfully");
   });
 }
 
