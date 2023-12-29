d329fb3a6085e2416a951369445c1aeabd03cb20
Mico Latorre Gaëtano <38907982+Gaet8802@users.noreply.github.com>
2023-12-29 16:14:57 +0100

    up: adresses for sokcet connection

--- frontend/src/Components/SingleChat.js
+++ frontend/src/Components/SingleChat.js
@@ -20,7 +20,7 @@ import io from "socket.io-client";
 import Lottie from "lottie-react";
 import animationData from "../animations/typing.json";
 
-const ENDPOINT = "http://localhost:5000";
+const ENDPOINT = "https://smalltalk-043u.onrender.com";
 
 var socket, selectedChatCompare;
 
